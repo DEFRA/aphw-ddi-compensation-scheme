45f39142e614bf223824ac64ba4374d532b3fbe5
sfitz42 <shaun.r.fitzsimons@gmail.com>
2023-10-27 10:06:43 +0100

    Fix test stage

--- jest.config.js
+++ jest.config.js
@@ -13,7 +13,8 @@ module.exports = {
     '<rootDir>/node_modules/',
     '<rootDir>/test-output/',
     '<rootDir>/test/',
-    '<rootDir>/jest.config.js'
+    '<rootDir>/jest.config.js',
+    '<rootDir>/app/dist'
   ],
   modulePathIgnorePatterns: [
     'node_modules'
